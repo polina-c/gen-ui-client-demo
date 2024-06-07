7b2afe1a13db730c2ddc5a544e7ba803f4141a52
Polina Cherkasova <polina.c@live.com>
2024-06-07 09:21:32 -0700

    -

--- rfw_renderer/lib/rfw.dart
+++ rfw_renderer/lib/rfw.dart
@@ -9,10 +9,15 @@ class RfwContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return SizedBox(
       width: 300,
-      height: 150,
-      child: Example(),
+      height: 180,
+      child: Column(
+        children: [
+          Text('$frameId'),
+          if (frameId == 'improved') ImprovedExample() else Example(),
+        ],
+      ),
     );
   }
 }
@@ -28,50 +33,66 @@ class _ExampleState extends State<Example> {
   final Runtime _runtime = Runtime();
   final DynamicContent _data = DynamicContent();
 
-  // @override
-  // void reassemble() {
-  //   // This function causes the Runtime to be updated any time the app is
-  //   // hot reloaded, so that changes to _createLocalWidgets can be seen
-  //   // during development. This function has no effect in production.
-  //   super.reassemble();
-  //   _update();
-  // }
+  // Normally this would be obtained dynamically, but for this example
+  // we hard-code the "remote" widgets into the app.
+  //
+  // Also, normally we would decode this with [decodeLibraryBlob] rather than
+  // parsing the text version using [parseLibraryFile]. However, to make it
+  // easier to demo, this uses the slower text format.
+  static final RemoteWidgetLibrary _remoteWidgets = parseLibraryFile('''
+    // The "import" keyword is used to specify dependencies, in this case,
+    // the built-in widgets that are added by initState below.
+    import core.widgets;
+    // The "widget" keyword is used to define a new widget constructor.
+    // The "root" widget is specified as the one to render in the build
+    // method below.
+    widget root = Container(
+      color: 0xFF002211,
+      child: Center(
+        child: Text(text: ["Hello, ", data.greet.name, "!"], textDirection: "ltr"),
+      ),
+    );
+  ''');
 
-  // static const LibraryName localName = LibraryName(<String>['local']);
-  // static const LibraryName remoteName = LibraryName(<String>['remote']);
+  static const LibraryName coreName = LibraryName(<String>['core', 'widgets']);
+  static const LibraryName mainName = LibraryName(<String>['main']);
 
-  // void _update() {
-  //   _runtime.update(localName, _createLocalWidgets());
-  //   // Normally we would obtain the remote widget library in binary form from a
-  //   // server, and decode it with [decodeLibraryBlob] rather than parsing the
-  //   // text version using [parseLibraryFile]. However, to make it easier to
-  //   // play with this sample, this uses the slower text format.
-  //   _runtime.update(remoteName, parseLibraryFile('''
-  //     import local;
-  //     widget root = GreenBox(
-  //       child: Hello(name: "World"),
-  //     );
-  //   '''));
-  // }
+  @override
+  void initState() {
+    super.initState();
+    // Local widget library:
+    _runtime.update(coreName, createCoreWidgets());
+    // Remote widget library:
+    _runtime.update(mainName, _remoteWidgets);
+    // Configuration data:
+    _data.update('greet', <String, Object>{'name': 'World'});
+  }
 
-  // static WidgetLibrary _createLocalWidgets() {
-  //   return LocalWidgetLibrary(<String, LocalWidgetBuilder>{
-  //     'GreenBox': (BuildContext context, DataSource source) {
-  //       return ColoredBox(
-  //         color: const Color(0xFF002211),
-  //         child: source.child(<Object>['child']),
-  //       );
-  //     },
-  //     'Hello': (BuildContext context, DataSource source) {
-  //       return Center(
-  //         child: Text(
-  //           'Hello, ${source.v<String>(<Object>["name"])}!',
-  //           textDirection: TextDirection.ltr,
-  //         ),
-  //       );
-  //     },
-  //   });
-  // }
+  @override
+  Widget build(BuildContext context) {
+    return RemoteWidget(
+      runtime: _runtime,
+      data: _data,
+      widget: const FullyQualifiedWidgetName(mainName, 'root'),
+      onEvent: (String name, DynamicMap arguments) {
+        // The example above does not have any way to trigger events, but if it
+        // did, they would result in this callback being invoked.
+        debugPrint('user triggered event "$name" with data: $arguments');
+      },
+    );
+  }
+}
+
+class ImprovedExample extends StatefulWidget {
+  const ImprovedExample({super.key});
+
+  @override
+  State<ImprovedExample> createState() => _ImprovedExampleState();
+}
+
+class _ImprovedExampleState extends State<ImprovedExample> {
+  final Runtime _runtime = Runtime();
+  final DynamicContent _data = DynamicContent();
 
   // Normally this would be obtained dynamically, but for this example
   // we hard-code the "remote" widgets into the app.
@@ -88,7 +109,7 @@ widget root = Container(
   child: Center(
     child: Button(
       child: Padding(
-        padding: [ 20.0 ],
+        padding: [ 10.0 ],
         child: Text(text: data.counter, style: {
           fontSize: 56.0,
           color: 0xFF000000,
